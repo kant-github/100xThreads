9451581fb6862a1bfbe8554b33eedc73b20fd9e1
kant-github <kantrishi7779@gmail.com>
2025-03-04 16:43:43 +0530

    Some docket related issues

--- apps/web/src/components/chat/messages/Messages.tsx
+++ apps/web/src/components/chat/messages/Messages.tsx
@@ -10,7 +10,6 @@ import { MouseDownEvent } from "emoji-picker-react/dist/config/config";
 import { HiOutlineDotsVertical } from "react-icons/hi";
 import MessageOptionsMenu from "@/components/ui/MessageOptionsMenu";
 import OrganizationRolesTickerRenderer from "@/components/utility/tickers/organization_roles_tickers/OrganizationRolesTickerRenderer";
-
 interface ReactionPayload {
     message_id: string;
     emoji: string;
